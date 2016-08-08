372caa902c49f315181fba6a2f4ef81347374a29
Felipe Sanchez Aristizabal <felipe.sanchez@Quipux.local>
2016-08-08 08:33:46 -0500

    first commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,31 @@
+import {Component} from "@angular/core";  
+import {GitHubService} from '../../services/github';
+import {NavController} from 'ionic-angular';  
+import {DetailsPage} from '../details/details'; 
+
+@Component({
+    templateUrl: 'build/pages/home/home.html',
+    providers: [GitHubService]
+})
+export class HomePage {  
+    public foundRepos;
+    public username;
+
+    constructor(private github: GitHubService,  
+            private nav: NavController) {
+}
+
+    getRepos() {
+        this.github.getRepos(this.username).subscribe(
+            data => {
+                this.foundRepos = data.json();
+            },
+            err => console.error(err),
+            () => console.log('getRepos completed')
+        );
+    }
+    
+    goToDetails(repo) {  
+    this.nav.push(DetailsPage, { repo: repo });
+}
+}
